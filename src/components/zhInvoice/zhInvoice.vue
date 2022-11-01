95cd4872d9b238f622d57a1f9a05b3ac6ce4e7d2
vixcity <2091283625@qq.com>
2022-11-01 17:19:22 +0800

    开票单据更改以及财务的一些更改

--- src/components/zhInvoice/zhInvoice.vue
+++ src/components/zhInvoice/zhInvoice.vue
@@ -1,85 +1,130 @@
 <template>
-  <div class="zhInvoice popup"
-    v-show="show">
+  <div class="zhInvoice popup" v-show="show">
     <div class="main">
       <div class="titleCtn">
-        <div class="text">{{type|filterType}}开票{{update?'修改':''}}</div>
-        <div class="closeCtn"
-          @click="close">
+        <div class="text">{{ type | filterType }}开票{{ update ? '修改' : '' }}</div>
+        <div class="closeCtn" @click="close">
           <i class="el-icon-close"></i>
         </div>
       </div>
       <div class="contentCtn">
         <div class="row">
           <div class="info">
-            <el-input disabled
-              placeholder="开票单位"
-              v-model="client_name">
+            <el-input disabled placeholder="开票单位" v-model="client_name">
               <template slot="prepend">开票单位</template>
             </el-input>
           </div>
         </div>
-        <div v-for="(item,index) in invoiceInfo.data"
-          :key="index">
-          <div class="blue"
-            style="margin-left:6px">开票单据{{index+1}}
-            <i class="el-icon-close hoverRed fr"
-              style="font-size:20px;cursor:pointer"
-              @click="invoiceInfo.data.length===1?$message.error('至少有一项'):$deleteItem(invoiceInfo.data,index)"></i>
+        <div v-for="(item, index) in invoiceInfo.data" :key="index">
+          <div class="blue" style="margin-left: 6px">
+            开票单据{{ index + 1 }}
+            <i
+              class="el-icon-close hoverRed fr"
+              style="font-size: 20px; cursor: pointer"
+              @click="
+                invoiceInfo.data.length === 1 ? $message.error('至少有一项') : $deleteItem(invoiceInfo.data, index)
+              "
+            ></i>
           </div>
           <div class="row">
             <div class="info">
-              <el-input disabled
-                placeholder="无单据开票"
-                v-model="item.doc_code">
+              <el-input disabled placeholder="无单据开票" v-model="item.doc_code"> </el-input>
+            </div>
+          </div>
+          <div class="row">
+            <div class="info">
+              <el-input placeholder="发票代码" v-model="item.invoice_number" @input="changeType($event, index)">
               </el-input>
             </div>
           </div>
           <div class="row">
             <div class="info">
-              <el-input placeholder="开票号码"
-                v-model="item.invoice_code">
-              </el-input>
+              <el-input placeholder="发票号码" v-model="item.invoice_code"> </el-input>
             </div>
           </div>
           <div class="row">
             <div class="info">
-              <el-input placeholder="开票金额(必填)"
+              <el-select v-model="item.type" placeholder="发票类型">
+                <el-option label="专票" value="专票"> </el-option>
+                <el-option label="普票" value="普票"> </el-option>
+              </el-select>
+            </div>
+          </div>
+          <div class="row">
+            <div class="info">
+              <el-autocomplete
+                class="inline-input"
+                v-model="item.tax_rate"
+                :fetch-suggestions="querySearch"
+                placeholder="税率（必填）"
+                @input="getAboutTaxPrice(index)"
+              >
+                <template slot="append">%</template>
+              </el-autocomplete>
+            </div>
+          </div>
+          <div class="row">
+            <div class="info">
+              <el-input
+                placeholder="开票金额（税价合计，必填）"
                 v-model="item.price"
-                @input="changeNumToPrice($event,index)">
+                @input="
+                  changeNumToPrice($event, index)
+                  getAboutTaxPrice(index)
+                "
+              >
                 <template slot="append">元</template>
               </el-input>
             </div>
           </div>
           <div class="row">
             <div class="info">
-              <el-input placeholder="数字金额(默认)"
-                v-model="item.hanPrice"
-                disabled>
+              <el-input placeholder="数字金额(默认)" v-model="item.hanPrice" disabled>
                 <template slot="append">元</template>
               </el-input>
             </div>
           </div>
           <div class="row">
             <div class="info">
-              <el-input placeholder="备注信息"
-                v-model="item.desc">
-              </el-input>
+              <el-input placeholder="税额" v-model="item.price_tax" disabled
+                ><template slot="append">元</template></el-input
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="info">
+              <el-input placeholder="不含税金额" v-model="item.price_no_tax" disabled
+                ><template slot="append">元</template></el-input
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="info">
+              <el-input placeholder="备注信息" v-model="item.desc"> </el-input>
             </div>
           </div>
         </div>
       </div>
       <div class="oprCtn">
-        <span class="btn borderBtn"
-          @click="close">取消</span>
-        <span class="btn backHoverOrange"
+        <span class="btn borderBtn" @click="close">取消</span>
+        <span
+          class="btn backHoverOrange"
           v-if="!update"
-          @click="$addItem(invoiceInfo.data,{
-            order_id: '', doc_code: '', rel_doc_id: '', reason: '', file_url: '', price: '' 
-          })">添加</span>
-        <span class="btn"
-          :class="{'backHoverBlue':!update,'backHoverOrange':update}"
-          @click="saveInvoice">{{update?'修改':'确认'}}</span>
+          @click="
+            $addItem(invoiceInfo.data, {
+              order_id: '',
+              doc_code: '',
+              rel_doc_id: '',
+              reason: '',
+              file_url: '',
+              price: ''
+            })
+          "
+          >添加</span
+        >
+        <span class="btn" :class="{ backHoverBlue: !update, backHoverOrange: update }" @click="saveInvoice">{{
+          update ? '修改' : '确认'
+        }}</span>
       </div>
     </div>
   </div>
@@ -99,6 +144,11 @@ interface invoiceInfo {
     rel_doc_id: number | string
     desc: string
     invoice_code: string
+    invoice_number: string
+    tax_rate: number | string
+    type: string
+    price_tax: number | string
+    price_no_tax: number | string
     price: string
     hanPrice?: string
   }>
@@ -158,7 +208,21 @@ export default Vue.extend({
         invoice_type: 1,
         doc_type: 0,
         client_id: '',
-        data: [{ order_id: '', doc_code: '', rel_doc_id: '', price: '', desc: '', invoice_code: '' }]
+        data: [
+          {
+            order_id: '',
+            doc_code: '',
+            rel_doc_id: '',
+            price: '',
+            desc: '',
+            invoice_code: '',
+            invoice_number: '',
+            tax_rate: '',
+            type: '专票',
+            price_tax: '',
+            price_no_tax: ''
+          }
+        ]
       }
     }
   },
@@ -204,7 +268,12 @@ export default Vue.extend({
               order_id: this.update ? item.order_id : this.type === 1 ? item.id : item.top_order_id,
               price: this.update ? item.price : '',
               desc: this.update ? item.desc : '',
-              invoice_code: this.update ? item.invoice_code : ''
+              invoice_code: this.update ? item.invoice_code : '',
+              invoice_number: this.update ? item.invoice_number : '',
+              tax_rate: this.update ? item.tax_rate : '',
+              type: this.update ? item.type : 1,
+              price_tax: this.update ? item.price_tax : '',
+              price_no_tax: this.update ? item.price_no_tax : ''
             }
           })
         } else {
@@ -215,7 +284,12 @@ export default Vue.extend({
               rel_doc_id: '',
               price: '',
               desc: '',
-              invoice_code: ''
+              invoice_code: '',
+              invoice_number: '',
+              tax_rate: '',
+              type: '专票',
+              price_tax: '',
+              price_no_tax: ''
             }
           ]
         }
@@ -242,6 +316,27 @@ export default Vue.extend({
         this.invoiceInfo.data[index].price = numStrArr.join('')
       }
     },
+    changeType(val: string, index: number) {
+      if (val.length === 12) {
+        this.invoiceInfo.data[index].type = '普票'
+      } else {
+        this.invoiceInfo.data[index].type = '专票'
+      }
+    },
+    querySearch(e: any, cb: any) {
+      cb([{ value: '0' }, { value: '3' }, { value: '6' }, { value: '9' }, { value: '13' }, { value: '17' }])
+    },
+    // 计算不含税金额 和 税额
+    getAboutTaxPrice(index: number) {
+      // 税额 = 税价合计 * 税率
+      // price_tax = price * tax_rate
+      // 不含税金额 =  税价合计 - 税额
+      // price_no_tax = price - price_tax
+      const item = this.invoiceInfo.data[index]
+      item.price_tax = (Number(item.price.replace(/[^0-9|.]/gi, '')) || 0) * (Number(item.tax_rate) / 100 || 0)
+      item.price_no_tax = (Number(item.price.replace(/[^0-9|.]/gi, '')) || 0) - (Number(item.price_tax) || 0)
+      console.log(item.price_tax, item.price_no_tax)
+    },
     close() {
       this.$emit('close')
     },
@@ -250,7 +345,21 @@ export default Vue.extend({
         invoice_type: this.invoice_type,
         doc_type: 0,
         client_id: '',
-        data: [{ order_id: '', doc_code: '', rel_doc_id: '', price: '', desc: '', invoice_code: '' }]
+        data: [
+          {
+            order_id: '',
+            doc_code: '',
+            rel_doc_id: '',
+            price: '',
+            desc: '',
+            invoice_code: '',
+            invoice_number: '',
+            tax_rate: '',
+            type: '专票',
+            price_tax: '',
+            price_no_tax: ''
+          }
+        ]
       }
     },
     saveInvoice() {
@@ -260,6 +369,10 @@ export default Vue.extend({
       }
       const formCheck = this.invoiceInfo.data.some((item) => {
         return this.$formCheck(item, [
+          {
+            key: 'tax_rate',
+            errMsg: '请输入税率'
+          },
           {
             key: 'price',
             errMsg: '请输入开票金额'
